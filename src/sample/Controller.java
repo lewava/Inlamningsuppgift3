8833976b1834b588f60e8e9d93eae247391d3be4
Leon Wass Vallin <leon.wass.vallin@mail.com>
2019-10-28 21:29:50 +0100

    Buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    public void swapButtons(Button button, Label empty) {
+        Integer temp  = GridPane.getRowIndex(button);
+        GridPane.setRowIndex(button, GridPane.getRowIndex(empty));
+        GridPane.setRowIndex(empty, temp);
+
+        temp = GridPane.getColumnIndex(button);
+        GridPane.setColumnIndex(button, GridPane.getColumnIndex(empty));
+        GridPane.setColumnIndex(empty, temp);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,6 +10,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Controller controller = new Controller();
     private Button button1 = new Button("1");
     private Button button2 = new Button("2");
     private Button button3 = new Button("3");
@@ -30,6 +32,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.addRow(0, button1, button2, button3, button4);
         gridPane.addRow(1, button5, button6, button7, button8);
         gridPane.addRow(2, button9, button10, button11, button12);
@@ -37,6 +40,52 @@ public class Main extends Application {
 
         gridPane.setHgap(10);
         gridPane.setVgap(10);
+
+        button1.setOnAction(event -> {
+            controller.swapButtons(button1, empty);
+        });
+        button2.setOnAction(event -> {
+            controller.swapButtons(button2, empty);
+        });
+        button3.setOnAction(event -> {
+            controller.swapButtons(button3, empty);
+        });
+        button4.setOnAction(event -> {
+            controller.swapButtons(button4, empty);
+        });
+        button5.setOnAction(event -> {
+            controller.swapButtons(button5, empty);
+        });
+        button6.setOnAction(event -> {
+            controller.swapButtons(button6, empty);
+        });
+        button7.setOnAction(event -> {
+            controller.swapButtons(button7, empty);
+        });
+        button8.setOnAction(event -> {
+            controller.swapButtons(button8, empty);
+        });
+        button9.setOnAction(event -> {
+            controller.swapButtons(button9, empty);
+        });
+        button10.setOnAction(event -> {
+            controller.swapButtons(button10, empty);
+        });
+        button11.setOnAction(event -> {
+            controller.swapButtons(button11, empty);
+        });
+        button12.setOnAction(event -> {
+            controller.swapButtons(button12, empty);
+        });
+        button13.setOnAction(event -> {
+            controller.swapButtons(button13, empty);
+        });
+        button14.setOnAction(event -> {
+            controller.swapButtons(button14, empty);
+        });
+        button15.setOnAction(event -> {
+            controller.swapButtons(button15, empty);
+        });
 
         button1.setMinSize(100,100);
         button2.setMinSize(100,100);
@@ -55,7 +104,7 @@ public class Main extends Application {
         button15.setMinSize(100,100);
         empty.setMinSize(100,100);
 
-        Scene root = new Scene(gridPane, 433,433);
+        Scene root = new Scene(gridPane, 450,450);
         primaryStage.setTitle("15 Game");
         primaryStage.sizeToScene();
         primaryStage.setScene(root);
